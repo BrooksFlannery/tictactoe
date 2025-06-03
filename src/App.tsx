418ab621fe5f8512f8a4699921c09fb3617c22d9
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-06-02 23:09:05 -0400

    ui pass and added sounds

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import { InitGameState, InitMatchState, move, type moveCoords } from './gameEngi
 function App() {
   const [matchState, setMatchState] = useState(InitMatchState());
   const [message,setMessage] = useState('');
+  const [shake, setShake] = useState(false);
 
   useEffect(()=> {
     checkMessage();
+    
     if(matchState.game.endstate !== null){
       resetGameState();
     }
@@ -38,38 +40,48 @@ function App() {
   }
 
   function handleClick(cellIndex:number, rowIndex:number){
+    setShake(true)
+    const sound = new Audio('../sounds/toyEdit1.mp3');
+    sound.playbackRate = .8+( Math.random() - .1);
+    sound.play();
     const coords : moveCoords = {
       rowIndex, colIndex:cellIndex
     };
     setMatchState(move(matchState,coords))
   }
 
-  return (
-    <>
-    <h1>Tic-Tac-Toe</h1>
-      <div className='board'>
-        {matchState.game.board.map((row, rowIndex) => {
-          return ( 
-            <div key = {rowIndex} className='row'> 
-              {row.map((cell, cellIndex) => {
-                return (
-                <div onClick={()=>handleClick(cellIndex, rowIndex)} className = "cell" key={cellIndex}> 
-                  {cell} 
-                </div>)
-              })
-            }
-            </div>)
-          })}
-      </div>
-    <div className='score-board'>
-        <div className='score o'>O: {matchState.oScore}</div>
-        <div className='score x'>X: {matchState.xScore}</div>
-    </div>    
-    <div className='end-state'>
-          {message}
-    </div>
+   useEffect(() => {
+    if (shake) {
+      const timeout = setTimeout(() => setShake(false), 400);
+      return () => clearTimeout(timeout);
+    }
+  }, [shake]);
 
-    </>
+  return (
+    <div className='game-screen'>
+      <h1>Tic-Tac-Toe</h1>
+        <div className={`board ${shake ? ['shake', 'rattle', 'roll'][Math.floor(Math.random() * 3)] : ''}`}>
+          {matchState.game.board.map((row, rowIndex) => {
+            return ( 
+              <div key = {rowIndex} className='row'> 
+                {row.map((cell, cellIndex) => {
+                  return (
+                  <div onClick={()=>handleClick(cellIndex, rowIndex)} className = "cell" key={cellIndex}> 
+                    {cell} 
+                  </div>)
+                })
+              }
+              </div>)
+            })}
+        </div>
+      <div className='score-board'>
+          <div className='score o'>O: {matchState.oScore}</div>
+          <div className='score x'>X: {matchState.xScore}</div>
+      </div>    
+      <div className='end-state'>
+            {message}
+      </div>
+    </div>
 
   )
 }
