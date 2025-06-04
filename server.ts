11e49e0dbd0894fc0c31a348cc303095d6388dfa
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-06-04 12:14:05 -0400

    connected server.js to my api

--- server.ts
+++ server.ts
@@ -1,8 +1,29 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { MemoryMatchAPI } from "./api";
 
 const app = express();
+app.use(express.json());
+const api = new MemoryMatchAPI();
 
-app.get("/message", (_, res) => res.send("Hello from express!"));
+
+//get game
+app.get('/api/match/:matchId', async (req,res) => {
+    const match = api.getMatch(req.params.matchId);
+    res.json(match)
+})
+
+//create game
+app.post('/api/match/', async (req,res) =>{
+    const match = api.createMatch();
+    res.json(match);
+})
+
+//make move
+app.post('/api/match/:matchId', async (req,res) => {
+    const match = api.makeMove(req.params.matchId, req.body.moveCoords);
+    res.json(match);
+})
+
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
