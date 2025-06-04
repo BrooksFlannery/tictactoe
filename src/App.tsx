6b8611ce01d88207c469d88f73cf47b0e69e0cc6
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-06-04 10:25:38 -0400

    misc fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,57 @@
 // import { useState } from 'react'
 
 import { useState, useEffect } from 'react';
-import { InitGameState, InitMatchState, move, type moveCoords } from './gameEngine'
+import { InitGameState, InitMatchState, move, type MoveCoords } from './gameEngine'
 
 function App() {
   const [matchState, setMatchState] = useState(InitMatchState());
   const [message,setMessage] = useState('');
   const [shake, setShake] = useState(false);
 
+  const successSounds = [
+      '../sounds/success/1.mp3',
+      '../sounds/success/2.mp3',
+      '../sounds/success/3.mp3',        
+      '../sounds/success/4.mp3',
+      '../sounds/success/5.mp3',
+    ];
+  const failSounds = [
+      '../sounds/failure/1.mp3',
+      '../sounds/failure/2.mp3',
+      '../sounds/failure/3.mp3',     
+    ]
+
   useEffect(()=> {
     checkMessage();
     
-    if(matchState.game.endstate !== null){
+    if(matchState.game.EndState === "x" || matchState.game.EndState === "o"){
+      
+      const randomSound = successSounds[Math.floor(Math.random() * successSounds.length)];
+
+      const sound = new Audio(randomSound);
+      sound.playbackRate = .8+( Math.random() - .1);
+      sound.play();
+      resetGameState();
+    }
+    if(matchState.game.EndState === 'tie'){
+      const randomSound = failSounds[Math.floor(Math.random() * failSounds.length)];
+
+      const sound = new Audio(randomSound);
+      sound.playbackRate = .8+( Math.random() - .1);
+      sound.play();
       resetGameState();
     }
   }, [matchState])  
 
   function resetGameState(){
    setTimeout(()=> {
-        setMatchState({...matchState, game: InitGameState()})
-        setMessage('')
+      setMessage('')
+      setMatchState({...matchState, game: InitGameState()})
     },2000)
 }
 
   function checkMessage(){
-      {switch(matchState.game.endstate){
+      {switch(matchState.game.EndState){
         case "x":
          setMessage("X's Win!!!")
         break;
@@ -44,7 +71,7 @@ function App() {
     const sound = new Audio('../sounds/toyEdit1.mp3');
     sound.playbackRate = .8+( Math.random() - .1);
     sound.play();
-    const coords : moveCoords = {
+    const coords : MoveCoords = {
       rowIndex, colIndex:cellIndex
     };
     setMatchState(move(matchState,coords))
@@ -61,7 +88,7 @@ function App() {
     <div className='game-screen'>
       <h1>Tic-Tac-Toe</h1>
         <div className={`board ${shake ? ['shake', 'rattle', 'roll'][Math.floor(Math.random() * 3)] : ''}`}>
-          {matchState.game.board.map((row, rowIndex) => {
+          {matchState.game.Board.map((row, rowIndex) => {
             return ( 
               <div key = {rowIndex} className='row'> 
                 {row.map((cell, cellIndex) => {
