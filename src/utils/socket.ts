449928d84e2d35f28902555660832d46f7ca0f38
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-06-08 23:34:01 -0400

    fixed socket bug, potentially fixed prod crashing bug, tbd shortly

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
 const URL = 'https://tictactoe-ux6o.onrender.com'//lazy and bad
+const DEV_URL = 'http://localhost:3000/'
 
-export const socket = io(`${URL}`);
+export const socket = io(`${DEV_URL}`, { autoConnect: false });
