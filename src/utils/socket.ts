b01f1cca12931a55441dbcbd5a2f8e85aba27030
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-06-08 23:35:47 -0400

    forgot to switch vars

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
 const URL = 'https://tictactoe-ux6o.onrender.com'//lazy and bad
-const DEV_URL = 'http://localhost:3000/'
 
-export const socket = io(`${DEV_URL}`, { autoConnect: false });
+export const socket = io(`${URL}`, { autoConnect: false });
